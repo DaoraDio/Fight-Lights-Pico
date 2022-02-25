6101f7a226aa57347cc2d34c36cb343c4d6d9717
DaoraDio <73956136+DaoraDio@users.noreply.github.com>
2022-02-25 14:33:23 +0100

    Bugfix
    Fixed a bug that would lead to a crash if only 1 config file was present and left was input during mode_select()

--- functions.py
+++ functions.py
@@ -447,7 +447,10 @@ def mode_select():
     config_name = 'config.py'
     while True:
         pixels_fill((255,255,255))
-        profile_color = get_profile_color(config_name)
+        if config_name != 'config0.py':
+            profile_color = get_profile_color(config_name)
+        else:
+            config_name = 'config.py'
         pixels_set(init.mode_selector, profile_color)
         if config.brightness_mod > 0.0:
             pixels_show(config.brightness_mod)
@@ -493,6 +496,7 @@ def mode_select():
                 
         if button.led_option.released or button.x.was_pressed: #confirm button
             if init.mode_selector == 0:
+                pixels_fill((0,0,0))
                 config_name = 'config.py'
                 return
             else:
